a1df0cbcd4e02959a8a79902ceba1c4560cfda7b
xl-xlxl <xlxl0495@outlook.com>
2025-07-28 15:09:52 +0800

    更改任务接口的响应格式

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -5,9 +5,9 @@ exports.getTasks = async (req, res, next) => {
     try {
         const result = await taskService.getTasks(req.query);
         if (!result.tasks || result.tasks.length === 0) {
-            return res.json({ message: '没有查询到相关任务', data: result });
+            return res.sucess(result, '没有查询到相关任务', 'NO_TASK');
         }
-        return res.json({ message: '查询成功', data: result });
+        return res.sucess(result, '查询成功', 'SUCCESS');
     } catch (error) {
         next(error); // 交给错误处理中间件
     }
