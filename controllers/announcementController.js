b5d13d5c05c981ec1fcf259ffde84cc062b72a43
Almighty <kz@gdmu.edu.cn>
2025-08-12 01:19:21 +0800

    规范接口返回

--- controllers/announcementController.js
+++ controllers/announcementController.js
@@ -18,7 +18,7 @@ exports.getAnnouncements = async (req, res, next) => {
     try {
         const result = await announcementService.getAnnouncements(req.query);
         if (!result.announcements || result.announcements.length === 0) {
-            return res.success(result, '没有查询到相关公告', 'NO_ANNOUNCEMENT');
+            return res.success(result, '没有查询到相关公告', 'ANNOUNCEMENT_NOT_FOUND');
         }
         return res.success(result, '查询成功', 'SUCCESS');
     } catch (error) {
